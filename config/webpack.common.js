181cf30b43a6c3c37131f5c3270ce8fe772c6798
Eharahe <7814324@qq.com>
2017-12-17 22:27:56 +0800

    add webpack config

---
+++ config/webpack.common.js
@@ -0,0 +1,68 @@
+var helpers = require('./helpers');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var a = require('@ngtools/webpack');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        // loaders: [
+        //   {
+        //     loader: 'awesome-typescript-loader',
+        //     options: { configFileName: helpers.root('tsconfig.json') }
+        //   } , 'angular2-template-loader'
+        // ]
+        loader: '@ngtools/webpack'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?sourceMap' })
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        loader: 'raw-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new a.AotPlugin({
+      tsConfigPath: helpers.root('tsconfig.json'),
+      entryModule:  helpers.root('src/app/', 'app.module#AppModule')
+    }),
+    // Workaround for angular/angular#11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)@angular/,
+      helpers.root('./src'), // location of your src
+      {} // a map of your routes
+    ),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+    new ExtractTextPlugin('[name].[hash].css')
+  ]
+};
+
---
+++ config/webpack.prod.js
@@ -0,0 +1,42 @@
+var helpers = require('./helpers');
+var webpack = require('webpack');
+var webpackMerge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: '/static/webaudio/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].chunk.js'
+  },
+
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
+      mangle: {
+        keep_fnames: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringify(ENV)
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      htmlLoader: {
+        minimize: false // workaround for ng2
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.prod.html',
+      filename: 'index.html'
+    })
+  ]
+});
+
